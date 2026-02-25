c2cf16fbd59f55026cd1cab91342401d295568ea
Yago <yagofh695@gmail.com>
2026-02-24 21:32:49 -0300

    Adicionado pasta mapas / fix colisão arenas

---
+++ Mapas_LigaDasTropas/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+this_is_a_map "yes"
+
+author 'Kraken Store'
+description 'Mapas LigaDasTropas'
+version '1.0.0'
+
+KrakenStore 'https://discord.gg/84JGyX2GeS'
+CompreMapas 'https://krakenstore.tebex.io/'
+
